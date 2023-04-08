92a2976772faff0e5674f68b34912dc55ad44ab5
Eric Liu <25liue03@stu.smuhsd.org>
2023-04-08 14:18:34 -0700

    qu

--- js/background.js
+++ js/background.js
@@ -1,5 +1,8 @@
+//getting the variables
 const mainP = require('./getpassage.js');
 const api = require('./config.js');
+
+//configuring openAIapikey
 const { Configuration, OpenAIApi } = require("openai");
 const passage = mainP
 const configuration = new Configuration({
@@ -7,6 +10,7 @@ apiKey: api,
 });
 const openai = new OpenAIApi(configuration);
 
+//function to return the passage with the most important parts "highlighted"
 async function return_important() {
 const completion = await openai.createCompletion({
 model: "text-davinci-003",
@@ -17,4 +21,5 @@ max_tokens: 200,
 console.log(completion.data.choices[0].text);
 }
 
+//run "return_important"
 return_important();
