edd9b7b98a8efe2c7412023846edf88db5e25726
smukhyala <smukhyala@gmail.com>
2023-04-08 20:09:19 -0700

    file organizing

---
+++ js/analyze.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+const analyzeButton = document.getElementById("analyzeButton");
+startAnalyze.addEventListener("click", function() {
+
+})
+});
