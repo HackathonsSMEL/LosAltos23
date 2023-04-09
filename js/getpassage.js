3e2ee18d3709cda386db4c585f10beff4f47c9d4
Eric Liu <25liue03@stu.smuhsd.org>
2023-04-09 03:09:28 -0700

    get

--- js/getpassage.js
+++ js/getpassage.js
@@ -9,9 +9,10 @@ document.addEventListener('mousedown', clearHighlight);
 function highlightText() {
   // Get the highlighted text and append it to the existing highlightedText string
   var text = window.getSelection().toString();
+  if (text != "") {
+    var text = document.getSelection().text;
+  }
     highlightedText += text; // add a newline character to separate multiple highlights
-    // Save the string to Chrome storage
-    chrome.storage.local.set({'highlightedText': highlightedText});
 }
 highlightText();
 module.exports = highlighhtedText;
@@ -21,7 +22,6 @@ module.exports = highlighhtedText;
 function clearHighlight() {
   // Clear the highlightedText string and remove it from Chrome storage
   highlightedText = '';
-  chrome.storage.local.remove('highlightedText');
 }
 
 /* 
